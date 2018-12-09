566a4dfdc5163e8d732519e4bad805a020a7b462
bryandms <bryandms@hotmail.com>
2018-12-09 13:08:18 -0600

    Setting up mongoose (mLab)

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(process.env.MongoDB_URI, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('Connected to dabase')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
